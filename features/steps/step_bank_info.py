0075f7d50450280217e28cd02e208606f259635b
Hossain <ahossain@apmelmac021.ad.ea.com>
2019-11-20 17:52:59 +1100

    Add parser for nullable string and add more test cases for account name

--- features/steps/step_bank_info.py
+++ features/steps/step_bank_info.py
@@ -1,11 +1,19 @@
-
 import json
-
+import parse
 import requests
+
 from assertpy import assert_that
-from behave import given, when, then, step
+from behave import given, when, then, step, register_type
 
 from config import CONFIG
+
+
+@parse.with_pattern(r'.*')
+def parse_nullable_string(text):
+    return text
+
+
+register_type(NullableString=parse_nullable_string)
 
 
 @given('A user makes a request to the endpoint "{endpoint}"')
@@ -19,37 +27,37 @@ def set_headers(context):
     context.headers = headers
 
 
-@step('with payment method "{payment_method}"')
+@step('with payment method "{payment_method:NullableString}"')
 def set_payment_method(context, payment_method):
     context.request_body = {"payment_method": payment_method}
 
 
-@step('with bank country code "{bank_country_code}"')
+@step('with bank country code "{bank_country_code:NullableString}"')
 def set_bank_country_code(context, bank_country_code):
     context.request_body["bank_country_code"] = bank_country_code
 
 
-@step('with account name "{account_name}"')
+@step('with account name "{account_name:NullableString}"')
 def set_account_name(context, account_name):
     context.request_body["account_name"] = account_name
 
 
-@step('with account number "{account_number}"')
+@step('with account number "{account_number:NullableString}"')
 def set_account_number(context, account_number):
     context.request_body["account_number"] = account_number
 
 
-@step('with swift code "{swift_code}"')
+@step('with swift code "{swift_code:NullableString}"')
 def set_swift_code(context, swift_code):
     context.request_body["swift_code"] = swift_code
 
 
-@step('with bsb code "{bsb}"')
+@step('with bsb code "{bsb:NullableString}"')
 def set_aba(context, bsb):
     context.request_body["bsb"] = bsb
 
 
-@step('with aba "{aba}"')
+@step('with aba "{aba:NullableString}"')
 def set_aba(context, aba):
     context.request_body["aba"] = aba
 
@@ -71,23 +79,3 @@ def check_status_code(context, status_code):
 def check_response_body(context, response_body):
     assert_that(context.response_body).is_equal_to(json.loads(response_body))
 
-#
-# @step('A user creates {country_code} record with account number {acc_number}')
-# def create_record(context, country_code, acc_number):
-#     context.execute_step(f'''
-#     Given A user makes a request to the endpoint "bank"
-#       And with headers
-#           """
-#           {
-#             "Cache-Control": "no-cache",
-#             "Content-Type": "application/json"
-#           }
-#           """
-#       And with payment method "SWIFT"
-#       And with bank country code "{country_code}"
-#       And with account name "Julia Roberts"
-#       And with account number "{acc_number}"
-#       And with swift code "ICBCUSBJ"
-#       And with aba "2232"
-#     When The user sends a post request
-#     ''')
